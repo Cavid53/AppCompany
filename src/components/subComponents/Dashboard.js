18aa96d1f0916c4c84c62e5343a4810175dcff74
Cavid Bashirov <bashirov.cavid@hotmail.com>
2020-12-23 19:46:27 +0400

    Completed search section

--- src/components/subComponents/Dashboard.js
+++ src/components/subComponents/Dashboard.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../../assets/styles/dashboard.scss";
 import DashboardHeader from "./DashboardHeader";
+import AppSearch from "./Search";
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
       <DashboardHeader />
       <div className="line-dashboard"></div>
+      <AppSearch/>
     </div>
   );
 };
